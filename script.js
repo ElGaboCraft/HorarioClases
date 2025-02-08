46a3f9ff28aae068d7a7de7a179bd6218aa956f4
ElGaboCraft <axgabriel0214@gmail.com>
2025-02-08 17:02:56 -0500

    Error Rendimiento por estrellas en celular solucionado deshabilitandolas en celulares.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const starsContainer = document.createElement("div");
-  starsContainer.className = "stars";
+  // Verifica si el ancho de la pantalla es mayor a 767.98px
+  if (window.innerWidth > 767.98) {
+    const starsContainer = document.createElement("div");
+    starsContainer.className = "stars";
 
-  for (let i = 0; i < 1000; i++) {
-    const star = document.createElement("span");
-    star.style.width = Math.random() * 3 + "px";
-    star.style.height = star.style.width;
-    star.style.left = Math.random() * 100 + "%";
-    star.style.top = Math.random() * 100 + "%";
-    star.style.animationDelay = Math.random() * 2 + "s";
-    starsContainer.appendChild(star);
+    for (let i = 0; i < 1000; i++) {
+      const star = document.createElement("span");
+      star.style.width = Math.random() * 3 + "px";
+      star.style.height = star.style.width;
+      star.style.left = Math.random() * 100 + "%";
+      star.style.top = Math.random() * 100 + "%";
+      star.style.animationDelay = Math.random() * 2 + "s";
+      starsContainer.appendChild(star);
+    }
+
+    document.body.prepend(starsContainer);
   }
-
-  document.body.prepend(starsContainer);
 });
